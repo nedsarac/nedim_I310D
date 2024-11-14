f64d8e7e8185a4316007b55736b57da93da580f8
Nedim Sarac <ns35557@utexas.edu>
2024-11-14 13:23:03 -0600

    final revision

--- volume_of_sphere.py
+++ volume_of_sphere.py
@@ -9,4 +9,4 @@ print(f"The volume of a sphere with radius {radius1} is: {volume1}")
 
 radius2 = 40
 volume2 = calculate_volume_of_sphere(radius2)
-print(f"The area of circle with radius {radius2} is: {volume2}")
+print(f"The volume of a sphere with radius {radius2} is: {volume2}")
